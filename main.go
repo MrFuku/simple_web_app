1665bdf9388ee530e31fdff20936e6a9ab202362
MrFuku <mmafighter_shun@icloud.com>
2020-03-12 23:01:30 +0900

    トップページ表示

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.temp1.Execute(w, nil)
+}
+
 func main() {
-	fmt.Println("Hello Go")
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
