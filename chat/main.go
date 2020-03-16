1b783f3187d76b8a4b2bb2ff0c1467600c247d5b
MrFuku <mmafighter_shun@icloud.com>
2020-03-16 12:28:42 +0900

    ログインユーザー名の表示

--- chat/main.go
+++ chat/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -25,7 +26,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.temp1.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.temp1.Execute(w, data)
 }
 
 func main() {
